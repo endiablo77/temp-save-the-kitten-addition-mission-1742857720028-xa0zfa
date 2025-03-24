f8fdb2b2207135b91b6823a63757ac58af7ae733
endiablo77 <laulebihan@outlook.fr>
2025-03-25 00:10:43 +0100

    Copy src/pages/Index.tsx from save-the-kitten-addition-mission

---
+++ src/pages/Index.tsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import CatRescueGame from "../components/CatRescueGame";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen w-full flex justify-center items-center">
+      <CatRescueGame />
+    </div>
+  );
+};
+
+export default Index;
